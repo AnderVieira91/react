0548f0f6abf5e81945fc855a13b11c4ed35cc15d
AnderVieira91 <ander.vieira91@gmail.com>
2023-07-10 14:42:22 -0300

    melhorias simples no código

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import { Navigate } from 'react-router-dom';
+import { useLocation, Navigate } from "react-router-dom";
 import { hasAuthParams } from "react-oidc-context";
 
 /**
